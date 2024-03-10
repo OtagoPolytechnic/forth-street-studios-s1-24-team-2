60b6b40c8599e2b4ce9995fc4814161d69080ebb
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-03-11 10:22:01 +1300

    3D objects move on mouse click

--- ForthStreetRecyclingGame/Assets/Scripts/DragObject.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/DragObject.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class DragObject : MonoBehaviour
 {
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                transform.position = new Vector3(hit.point.x, hit.point.y, 0);
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = Color.white;
     }
 }
