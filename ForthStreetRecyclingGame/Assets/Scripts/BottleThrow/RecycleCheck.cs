9bb786e56fd44c4c1d10d9e09ee7b5c197698dd4
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-03-16 17:59:44 +1300

    FIX: Trigger areas now apply for all object types and will properly check rubbish/recycle

--- ForthStreetRecyclingGame/Assets/Scripts/BottleThrow/RecycleCheck.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/BottleThrow/RecycleCheck.cs
@@ -1,31 +1,58 @@
 using UnityEngine;
 
 /// <summary>
-/// Checks the tag of object during collision and marks as correct(green)/incorrect(red)
+/// Checks the tag of object during collision against the trigger tag.
+/// Marks as correct(green) / incorrect(red)
 /// </summary>
-/*
-*  Colour change of the objects is temporary for now and will update with more functionality later.
-*  Useful for prototyping stage
-*/
 public class RecycleCheck : MonoBehaviour
 {
     Renderer rend;
 
-    void OnTriggerEnter(Collider other) // When thrown object enters the trigger
+    // When thrown object enters the trigger, sets the colour of the object depending on the trigger tag
+    /*
+    *  Colour change of the objects is temporary for now and will update with more functionality later.
+    *  Useful for prototyping stage
+    */
+    void OnTriggerEnter(Collider other) 
     {
         rend = other.GetComponent<Renderer>();
+        Color colourToSet = Color.grey; //Set to temporary colour before check
 
-        // Check the tag of the collided object
-        switch (other.tag)
+        //Check tag of the trigger area (Recycling/Rubbish)
+        if (this.tag == "Recycling")
         {
-            case "Recycling":
-                rend.material.color = Color.green;
-                break;
-            case "Rubbish":
-                rend.material.color = Color.red;
-                break;
-            default:
-                break;
+            switch (other.tag)// Check the tag of the collided object
+            {
+                case "Recycling":
+                    colourToSet = Color.green;
+                    break;
+                case "Rubbish":
+                    colourToSet = Color.red;
+                    break;
+                default:
+                    break;
+            }
         }
+        else if (this.tag == "Rubbish")
+        {
+            // Check the tag of the collided object
+            switch (other.tag)
+            {
+                case "Recycling":
+                    colourToSet = Color.red;
+                    break;
+                case "Rubbish":
+                    colourToSet = Color.green;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else //Destroy any items that miss the main 2 trigger areas
+        {
+            Destroy(other.gameObject);
+        }
+
+        rend.material.color = colourToSet;
     }
 }
