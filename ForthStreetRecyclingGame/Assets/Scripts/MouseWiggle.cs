e21c150ab18a3654fdf33a1724ed702230c77eee
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-03-18 14:54:26 +1300

    Added progress bar slider

--- ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseWiggle : MonoBehaviour
 {
+    private GameObject lid;
+    [SerializeField]private Image bar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lid = GameObject.Find("Lid");
     }
 
     // Update is called once per frame
@@ -18,17 +22,25 @@ public class MouseWiggle : MonoBehaviour
 
     private void MouseMovement()
     {
-        if (Input.GetAxis("Mouse X") < 0)
+        if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0)
         {
-            Debug.Log("Mouse moved left");
-        }
-        if (Input.GetAxis("Mouse X") > 0)
-        {
-            Debug.Log("Mouse moved right");
+            //Debug.Log("Mouse moved");
+            //LidMovement();
+            BarMovement();
         }
         if (Input.GetAxis("Mouse X") == 0)
         {
-            Debug.Log("Mouse stopped");
+            //Debug.Log("Mouse stopped");
         }
+    }
+
+    private void LidMovement()
+    {
+        lid.transform.position = Vector3.MoveTowards(lid.transform.position, new Vector3(lid.transform.position.x, lid.transform.position.y + Time.deltaTime, lid.transform.position.z), Time.deltaTime * 0.01f);
+    }
+
+    private void BarMovement()
+    {
+        
     }
 }
