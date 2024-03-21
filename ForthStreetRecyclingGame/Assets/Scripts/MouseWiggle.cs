59c8f1b65522ae9db894c6abab0e7d4d44eefc8c
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-03-21 13:51:38 +1300

    Added lid rotation

--- ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
@@ -24,6 +24,7 @@ public class MouseWiggle : MonoBehaviour
         {
             Debug.Log("You win!");
         }
+        StartCoroutine(ShakeMeter());
     }
 
     private void MouseMovement()
@@ -31,12 +32,10 @@ public class MouseWiggle : MonoBehaviour
         if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0)
         {
             MeterUp();
-            StartCoroutine(ShakeMeter());
         }
         if (Input.GetAxis("Mouse X") == 0)
         {
             MeterDown();
-            StopCoroutine(ShakeMeter());
         }
     }
 
@@ -44,22 +43,28 @@ public class MouseWiggle : MonoBehaviour
     {
         fill.value = Mathf.MoveTowards(fill.value, 1, Time.deltaTime * 0.1f);
         lid.transform.position = Vector3.MoveTowards(lid.transform.position, new Vector3(lid.transform.position.x, lid.transform.position.y + Time.deltaTime, lid.transform.position.z), Time.deltaTime * 0.01f);
+        lid.transform.Rotate(new Vector3(0, 20f, 0) * Time.deltaTime);
     }
 
     private void MeterDown()
     {
         fill.value = Mathf.MoveTowards(fill.value, 0, Time.deltaTime * 0.1f);
         lid.transform.position = Vector3.MoveTowards(lid.transform.position, new Vector3(lid.transform.position.x, -lid.transform.position.y + Time.deltaTime, lid.transform.position.z), Time.deltaTime * 0.01f);
+        lid.transform.Rotate(new Vector3(0, 20f, 0) * Time.deltaTime);
     }
 
     private IEnumerator ShakeMeter()
     {
-        if (fill.value > 5)
+        if (fill.value > 0.5)
         {
             meter.transform.rotation = Quaternion.Euler(0, 0, 91);
             yield return new WaitForSeconds(0.5f);
             meter.transform.rotation = Quaternion.Euler(0, 0, 89);
             yield return new WaitForSeconds(0.5f);
+        }
+        else
+        {
+            meter.transform.rotation = Quaternion.Euler(0, 0, 90);
         }
     }
 }
