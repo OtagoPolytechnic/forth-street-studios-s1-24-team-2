d64a36bba4a54276f519a899702ee286df9925da
Devon Henderson (1000103794) <PARTDA1@student.op.ac.nz>
2024-03-21 14:38:53 +1300

    ADD: Extra changes to spawning script

--- ForthStreetRecyclingGame/Assets/Scripts/BBall Minigame/ObjectSpawner.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/BBall Minigame/ObjectSpawner.cs
@@ -1,11 +1,22 @@
+/*
+ * This script is currently just rough ideas and 
+ * brainstorming how to make the random spawns of
+ * items feel fair to the player. None of this 
+ * code 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
+    //At least 
     public List<GameObject> recycling;
     public List<GameObject> rubbish;
+
+    public GameObject lastSpawned;
+    public GameObject nextSpawned;
 
     public Vector3 spawnLocation;
     public ThrowBottle tb;
@@ -30,6 +41,7 @@ public class ObjectSpawner : MonoBehaviour
         if (!itemSpawned)
         {
             //Spawn in an item
+            //Select next item to spawn (ensure different to first item)
             //Set itemSpawned to true
         }
 
@@ -37,6 +49,25 @@ public class ObjectSpawner : MonoBehaviour
         {
             //Wait for .5 seconds and spawn another item
         }
+    }
 
+    void RandomObjectChoice(GameObject lastSpawned)
+    {
+        switch (lastSpawned)
+        {
+            case recycling[0].gameObject:
+            case recycling[1].gameObject:
+            case recycling[2].gameObject:
+                //increase chance to spawn in rubbish
+                break;
+
+            case rubbish[0].gameObject:
+            case rubbish[1].gameObject:
+            case rubbish[2].gameObject:
+                //increase chance to spawn in recycling
+                break;
+        }
+
+        //Randomly spawn from list based on spawn chance
     }
 }
