59a918a066991a46a092cf4013cda864f9eb9ecb
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-03-21 13:31:26 +1300

    Lid moves up and down based on movement

--- ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/MouseWiggle.cs
@@ -7,6 +7,7 @@ public class MouseWiggle : MonoBehaviour
 {
     private GameObject lid;
     [SerializeField]private Slider fill;
+    [SerializeField]private GameObject meter;
 
     // Start is called before the first frame update
     void Start()
@@ -19,35 +20,46 @@ public class MouseWiggle : MonoBehaviour
     void Update()
     {
         MouseMovement();
+        if (fill.value == 1)
+        {
+            Debug.Log("You win!");
+        }
     }
 
     private void MouseMovement()
     {
         if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0)
         {
-            //Debug.Log("Mouse moved");
-            //LidMovement();
-            BarMovement();
+            MeterUp();
+            StartCoroutine(ShakeMeter());
         }
         if (Input.GetAxis("Mouse X") == 0)
         {
-            //Debug.Log("Mouse stopped");
-            BarEmpty();
+            MeterDown();
+            StopCoroutine(ShakeMeter());
         }
     }
 
-    private void LidMovement()
+    private void MeterUp()
     {
+        fill.value = Mathf.MoveTowards(fill.value, 1, Time.deltaTime * 0.1f);
         lid.transform.position = Vector3.MoveTowards(lid.transform.position, new Vector3(lid.transform.position.x, lid.transform.position.y + Time.deltaTime, lid.transform.position.z), Time.deltaTime * 0.01f);
     }
 
-    private void BarMovement()
-    {
-        fill.value = Mathf.MoveTowards(fill.value, 1, Time.deltaTime * 0.08f);
-    }
-
-    private void BarEmpty()
+    private void MeterDown()
     {
         fill.value = Mathf.MoveTowards(fill.value, 0, Time.deltaTime * 0.1f);
+        lid.transform.position = Vector3.MoveTowards(lid.transform.position, new Vector3(lid.transform.position.x, -lid.transform.position.y + Time.deltaTime, lid.transform.position.z), Time.deltaTime * 0.01f);
+    }
+
+    private IEnumerator ShakeMeter()
+    {
+        if (fill.value > 5)
+        {
+            meter.transform.rotation = Quaternion.Euler(0, 0, 91);
+            yield return new WaitForSeconds(0.5f);
+            meter.transform.rotation = Quaternion.Euler(0, 0, 89);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
