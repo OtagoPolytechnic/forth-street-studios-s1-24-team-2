fd3b048ffe5a48e8c8089c2213ace06216dd5910
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-04-04 12:49:22 +1300

    If statements are now checked in update before starting a coroutine for optimisation

--- ForthStreetRecyclingGame/Assets/Scripts/FillMeter.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/FillMeter.cs
@@ -12,40 +12,50 @@ public class FillMeter : MonoBehaviour
     private Rigidbody lidRb;
 
     //Const variables
-    private const int MAX = 1;
-    private const int LEFTSHAKE = 89;
+    private const int MAX = 1; // slider scale is measured from 0 --> 1
+    private const int LEFTSHAKE = 89; // rotates the meter slightly to shake it
     private const int RIGHTSHAKE = 91;
-    private const int HORIZONTAL = 90;
-    private const int LIDPULL = 10;
+    private const int VERTICAL = 90; // used to stand the meter vertically
+    private const int LIDPULL = 10; // used in add force to throw the lid into the air
     private const int ANIMATIONTIME = 2;
-    private const float SHAKESPEED = 0.5f;
-    private const float FILLSPEED = 0.3f;
-    private const float EMPTYSPEED = 0.08f;
+    private const float SHAKESPEED = 0.5f; // wait time between shakes
+    private const float FILLSPEED = 0.3f; // how much the meter fills
+    private const float EMPTYSPEED = 0.08f; // how much the meter empties
 
     // Start is called before the first frame update
     void Start()
     {
-        fill.value = 0;
+        fill.value = 0; //meter starts at 0
     }
 
     // Update is called once per frame
     void Update()
     {
         MouseMovement();
-        if (fill.value == MAX) // if the meter is full
+        if (fill.value == MAX) //  if the meter is full
         {
-            StartCoroutine(LidRemoval());
+            if (bottle.transform.GetChild(0).gameObject.GetComponent<Rigidbody>() == false) //checks first to see if there's already a rigidbody on the lid
+            {
+                StartCoroutine(LidRemoval());
+            }
         }
-        StartCoroutine(ShakeMeter());
+        else if(fill.value > SHAKESPEED) // checking the meter is half filled
+        {
+            StartCoroutine(ShakeMeter());
+        }
+        else //the meter stays horizontal the rest of the time
+        {
+            meter.transform.rotation = Quaternion.Euler(0, 0, VERTICAL);
+        }
     }
 
     private void MouseMovement()
     {
-        if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0)
+        if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse X") > 0) // mouse moving left and right
         {
             MeterUp();
         }
-        if (Input.GetAxis("Mouse X") == 0)
+        if (Input.GetAxis("Mouse X") == 0) // mouse is still
         {
             MeterDown();
         }
@@ -64,28 +74,18 @@ public class FillMeter : MonoBehaviour
     //Shake the meter when it's getting close to the top
     private IEnumerator ShakeMeter()
     {
-        if (fill.value > SHAKESPEED)
-        {
-            meter.transform.rotation = Quaternion.Euler(0, 0, RIGHTSHAKE);
-            yield return new WaitForSeconds(SHAKESPEED);
-            meter.transform.rotation = Quaternion.Euler(0, 0, LEFTSHAKE);
-            yield return new WaitForSeconds(SHAKESPEED);
-        }
-        else
-        {
-            meter.transform.rotation = Quaternion.Euler(0, 0, HORIZONTAL);
-        }
+        meter.transform.rotation = Quaternion.Euler(0, 0, RIGHTSHAKE);
+        yield return new WaitForSeconds(SHAKESPEED);
+        meter.transform.rotation = Quaternion.Euler(0, 0, LEFTSHAKE);
+        yield return new WaitForSeconds(SHAKESPEED);
     }
 
     //Removes the lid from the bottle as if the player has fished it up!
     private IEnumerator LidRemoval()
     {
-        if (bottle.transform.GetChild(0).gameObject.GetComponent<Rigidbody>() == false)
-        {
-            lidRb = bottle.transform.GetChild(0).gameObject.AddComponent<Rigidbody>(); // adds rigidbody to the lid
-            lidRb.useGravity = false; // disables gravity
-            lidRb.AddForce(transform.up * LIDPULL); // adds rigidbody to the lid
-        }
+        lidRb = bottle.transform.GetChild(0).gameObject.AddComponent<Rigidbody>(); // adds rigidbody to the lid
+        lidRb.useGravity = false; // disables gravity
+        lidRb.AddForce(transform.up * LIDPULL); // adds rigidbody to the lid
         yield return new WaitForSeconds(ANIMATIONTIME);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reloads scene (temporary)
 
