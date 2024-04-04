4d651403fa25454bffc89a4f810b864285554a8b
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-04-04 14:36:20 +1300

    Readded rotate script

--- ForthStreetRecyclingGame/Assets/Scripts/Rotate.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/Rotate.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+/*
+ * File: Rotate.cs
+ * Purpose: Basic rotation script for objects
+ * Author: Johnathan
+ * Contributions: Assisted by GitHub Copilot
+ */
+
 using UnityEngine;
 
+/// <summary>
+/// This class is used to rotate an object.
+/// </summary>
+/// <remarks>
+/// This should be refactored to work on a provided axis.
+/// </remarks>
 public class Rotate : MonoBehaviour
 {
     public float RotationSpeed = 50f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    /// <summary>
+    /// Perform the rotation
+    /// </summary>
     void Update()
     {
         transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
