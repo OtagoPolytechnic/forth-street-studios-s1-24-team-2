93652144962dc3690a661acd660a23b082e481f1
Yousef Altay (1000070390) <ALTAYY1@student.op.ac.nz>
2024-04-08 13:36:00 +1200

    started new mini-game

---
+++ ForthStreetRecyclingGame/Assets/Scripts/GridManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    public List<Sprite> Sprites = new List<Sprite>();
+    public GameObject TilePrefab;
+    public int GridDimension = 8;
+    public float Distance = 1.0f;
+    private GameObject[,] Grid;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
