a5d0e40f54a60522d59a7ba7a51f493c55bd2d8c
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-02 14:10:14 +1200

    ADD: Materials and clicking the bin (temporarily) prints debug

--- ForthStreetRecyclingGame/Assets/Scripts/Bin Snap/SnapItemToBin.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/Bin Snap/SnapItemToBin.cs
@@ -7,7 +7,7 @@ public class SnapItemToBin : MonoBehaviour
     private Vector3 wayPoint;
     public GameObject heldItem;
     private DragObject dragObject; //script reference to the drag object
-
+    private string binType;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,47 @@ public class SnapItemToBin : MonoBehaviour
         wayPoint = transform.GetChild(0).gameObject.transform.position; //gets the first child of the bin
     }
 
+    void Update()
+    {
+        //GetMouseButtonDown doesn't work in a method that is a mouse event itself, so it's moved to Update and type is stored
+        if (Input.GetMouseButtonDown(0) && binType != null) // if the player clicks a bin
+        {
+            BinCheck(); // checks the type
+        }
+    }
+
     void OnMouseEnter()
     {
+        if (dragObject.item == null) return; //if the item is null, return (do nothing)
         heldItem = dragObject.item; //gets the item from the drag object
         heldItem.transform.position = wayPoint; //snaps the item to the bin
+        binType = gameObject.name;
+    }
+
+    void OnMouseExit()
+    {
+        binType = null;
+    }
+
+    private void BinCheck()
+    {
+        switch (binType)
+        {
+            case "Recycling":
+                Debug.Log("Recycling");
+                binType = "Recycling";
+                // Minigame
+                return;
+            case "Rubbish":
+                Debug.Log("Rubbish");
+                binType = "Rubbish";
+                Destroy(heldItem);
+                return;
+            case "Glass":
+                Debug.Log("Glass");
+                binType = "Glass";
+                // Minigame
+                return;
+        }
     }
 }
