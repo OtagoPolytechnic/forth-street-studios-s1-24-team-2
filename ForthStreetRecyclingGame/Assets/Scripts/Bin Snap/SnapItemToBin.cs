9e70f5ad1d3d047866db0c13a8d9996dd309b127
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-02 14:59:50 +1200

    FIX: Item offset in bin

--- ForthStreetRecyclingGame/Assets/Scripts/Bin Snap/SnapItemToBin.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/Bin Snap/SnapItemToBin.cs
@@ -2,42 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Snaps the item to the bin when the mouse enters the bin
+/// </summary>
 public class SnapItemToBin : MonoBehaviour
 {
     private Vector3 wayPoint;
+    private float wpYOffset;
     public GameObject heldItem;
     private DragObject dragObject; //script reference to the drag object
     private string binType;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Gets scripts and objects when the scene first loads
+    /// </summary>
     void Start()
     {
         dragObject = GameObject.Find("PickupObjects").GetComponent<DragObject>(); //finds the pickup object and gets the script
         wayPoint = transform.GetChild(0).gameObject.transform.position; //gets the first child of the bin
+        wpYOffset = 0.75f; //offsets the item to the bin
+        wayPoint.y += wpYOffset; //offsets the item to the bin
     }
 
+    /// <summary>
+    /// GetMouseButtonDown doesn't work in a method that is a mouse event itself, so it's moved to Update and type is stored
+    /// </summary>
     void Update()
     {
-        //GetMouseButtonDown doesn't work in a method that is a mouse event itself, so it's moved to Update and type is stored
         if (Input.GetMouseButtonDown(0) && binType != null) // if the player clicks a bin
         {
             BinCheck(); // checks the type
         }
     }
 
+    /// <summary>
+    /// When the mouse enters the bin, the item is snapped to the bin
+    /// </summary>
     void OnMouseEnter()
     {
         if (dragObject.item == null) return; //if the item is null, return (do nothing)
         heldItem = dragObject.item; //gets the item from the drag object
         heldItem.transform.position = wayPoint; //snaps the item to the bin
+        heldItem.transform.rotation = Quaternion.Euler(90,0,0); //rotates the item to the bin
         binType = gameObject.name;
     }
 
+    /// <summary>
+    /// When the mouse exits the bin, the bin type is set to null
+    /// so the player can't press mouse button outside of the bin
+    /// </summary>
     void OnMouseExit()
     {
         binType = null;
     }
 
+    /// <summary>
+    /// Checks the bin type
+    /// </summary>
     private void BinCheck()
     {
         switch (binType)
