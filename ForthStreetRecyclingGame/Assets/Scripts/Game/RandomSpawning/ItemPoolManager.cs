52767a667f39e6fb20c4f1ba00752d65f0292125
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-05-18 17:56:31 +1200

    FIX: Items now spawn as seperate lists for recycle/rubbish

--- ForthStreetRecyclingGame/Assets/Scripts/Game/RandomSpawning/ItemPoolManager.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/Game/RandomSpawning/ItemPoolManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class ItemPoolManager : MonoBehaviour
 {
-    public static ItemPoolManager Instance;
-    public List<GameObject> itemPrefabs;
-    public int poolSize = 20;
-    private List<GameObject> objectPool;
+    [SerializeField] private static ItemPoolManager Instance;
+    [SerializeField] private List<GameObject> recycleItemPrefabs;
+    [SerializeField] private List<GameObject> rubbishItemPrefabs;
+
+    private List<GameObject> recycleObjectPool;
+    private List<GameObject> rubbishObjectsPool;
+    private int poolSize = 20;
 
     void Awake()
     {
@@ -18,24 +21,31 @@ public class ItemPoolManager : MonoBehaviour
 
     void InitialisePool()
     {
-        objectPool = new List<GameObject>();
-        int maxOfItem = poolSize/itemPrefabs.Count;
-        Debug.Log(maxOfItem);
+        recycleObjectPool = new List<GameObject>();
+        rubbishObjectsPool = new List<GameObject>();
+
+        InstantiateObjects(recycleObjectPool, recycleItemPrefabs);
+        InstantiateObjects(rubbishObjectsPool, rubbishItemPrefabs);
+    }
+
+    void InstantiateObjects(List<GameObject> pool, List<GameObject> itemPrefabs)
+    {
+        int maxSpawnCount = poolSize / itemPrefabs.Count;
 
         foreach (GameObject itemPrefab in itemPrefabs)
         {
-            for (int i = 0; i < maxOfItem; i++)
+            for (int i = 0; i < maxSpawnCount; i++)
             {
                 GameObject obj = Instantiate(itemPrefab);
                 obj.SetActive(false);
-                objectPool.Add(obj);
+                pool.Add(obj);
             }
         }
     }
 
-    public GameObject GetPooledObject()
+    public GameObject GetPooledObject(List<GameObject> pool)
     {
-        foreach (GameObject obj in objectPool)
+        foreach (GameObject obj in pool)
         {
             if (!obj.activeInHierarchy)
             {
