ae5aa2eed58b4693d29c66432ce3570d170d84f3
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-01 02:17:38 +1200

    feat: do not repeat mingame when choosing randomly

--- ForthStreetRecyclingGame/Assets/Scripts/MinigameLauncher/MinigameObjectManager.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/MinigameLauncher/MinigameObjectManager.cs
@@ -26,6 +26,7 @@ public class MinigameObjectManager : MonoBehaviour
 {
     // list of minigame object compositions, these should be assigned in the inspector
     public List<MinigameObjectComposition> minigameObjectCompositions = new();
+    private Minigame lastRandomMinigame;
 
     #region Singleton
     public static MinigameObjectManager instance;
@@ -61,6 +62,12 @@ public class MinigameObjectManager : MonoBehaviour
 
     public Minigame GetRandomMinigame()
     {
-        return minigameObjectCompositions[UnityEngine.Random.Range(0, minigameObjectCompositions.Count)].minigame;
+        Minigame game = minigameObjectCompositions[UnityEngine.Random.Range(0, minigameObjectCompositions.Count)].minigame;
+        if (minigameObjectCompositions.Count > 1 && game == lastRandomMinigame)
+        {
+            return GetRandomMinigame();
+        }
+        return game;
     }
+
 }
