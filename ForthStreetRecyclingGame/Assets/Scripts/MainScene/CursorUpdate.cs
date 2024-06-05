fa81dd63c296ee87ed4a6bea899603391182cb63
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-05 19:24:14 +1200

    FIX: CursorUpdate script now properly swaps cursor

--- ForthStreetRecyclingGame/Assets/Scripts/MainScene/CursorUpdate.cs
+++ ForthStreetRecyclingGame/Assets/Scripts/MainScene/CursorUpdate.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
 
-public class CursorUpdate : MonoBehaviour
+public class CursorManager : MonoBehaviour
 {
     public Texture2D defaultCursor;
     public Texture2D clickCursor;
+    private Vector2 cursorHotspotDefault;
+    private Vector2 cursorHotspotClick;
+
+    private void Start()
+    {
+        // Set the cursor hotspot to the center of the textures
+        cursorHotspotDefault = new Vector2(defaultCursor.width / 2, defaultCursor.height / 2);
+        cursorHotspotClick = new Vector2(clickCursor.width / 2, clickCursor.height / 2);
+
+        // Set the default cursor initially
+        SetDefaultCursor();
+    }
 
     private void Update()
     {
+        // Check if the left mouse button is being clicked and held
         if (Input.GetMouseButton(0))
         {
-            SetCursor(clickCursor);
+            SetClickCursor();
         }
         else
         {
-            SetCursor(defaultCursor);
+            SetDefaultCursor();
         }
     }
 
-    private void SetCursor(Texture2D cursor)
+    private void SetDefaultCursor()
     {
-        Vector2 hotspot = new Vector2(cursor.width / 2, cursor.height / 2); //Where the click is registered on the texture [ Centred ]
-        Cursor.SetCursor(defaultCursor, hotspot, CursorMode.Auto);
+        Cursor.SetCursor(defaultCursor, cursorHotspotDefault, CursorMode.Auto);
+    }
+
+    private void SetClickCursor()
+    {
+        Cursor.SetCursor(clickCursor, cursorHotspotClick, CursorMode.Auto);
     }
 }
